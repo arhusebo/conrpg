39733d9ef6e6dd9e08731bee8404a3e4084a3aec
eivn <2039@users.noreply.github.com>
2018-06-20 12:06:44 +0200

    Add listener.py

---
+++ listener.py
@@ -0,0 +1,19 @@
+def get_key(string='', echo=False):
+    import msvcrt
+    print(string, end='')
+    while True:
+        if msvcrt.kbhit():
+            key = msvcrt.getwch()
+            if key in u'\x00\xe0':  # arrow or function key prefix?
+                key = msvcrt.getwch()  # second call returns the actual key code
+                key = {
+                    'H' : 'w',
+                    'K' : 'a',
+                    'P' : 's',
+                    'M' : 's',
+                }.get(key)
+            if echo:
+                msvcrt.putwch(key)
+            return {
+                '\x1b' : 'ESC',
+            }.get(key, key)
