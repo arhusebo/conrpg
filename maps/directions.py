8a44f71f3ce01bd725b59b0e65f725941802df05
eivn <2039@users.noreply.github.com>
2018-06-26 18:41:55 +0200

    Fix East-West direction

--- maps/directions.py
+++ maps/directions.py
@@ -2,9 +2,9 @@
 from collections import namedtuple
 Direction = namedtuple("Direction", ['x', 'y'])
 NORTH = Direction(x =  0, y = -1)
-EAST  = Direction(x = -1, y =  0)
+EAST  = Direction(x =  1, y =  0)
 SOUTH = Direction(x =  0, y =  1)
-WEST  = Direction(x =  1, y =  0)
+WEST  = Direction(x = -1, y =  0)
 DIRECTIONS = {NORTH, EAST, SOUTH, WEST}
 
 def opposite(direction):
@@ -19,19 +19,19 @@ def symbol(dirs):
     #East and west are reversed ??
     return {
         (0,0,0,0) : ' ',
-        (0,0,0,1) : '╞',
+        (0,0,0,1) : '╡',
         (0,0,1,0) : '╥',
-        (0,0,1,1) : '╔',
-        (0,1,0,0) : '╡',
+        (0,0,1,1) : '╗',
+        (0,1,0,0) : '╞',
         (0,1,0,1) : '═',
-        (0,1,1,0) : '╗',
+        (0,1,1,0) : '╔',
         (0,1,1,1) : '╦',
         (1,0,0,0) : '╨',
-        (1,0,0,1) : '╚',
+        (1,0,0,1) : '╝',
         (1,0,1,0) : '║',
-        (1,0,1,1) : '╠',
-        (1,1,0,0) : '╝',
+        (1,0,1,1) : '╣',
+        (1,1,0,0) : '╚',
         (1,1,0,1) : '╩',
-        (1,1,1,0) : '╣',
+        (1,1,1,0) : '╠',
         (1,1,1,1) : '╬',
     }.get((NORTH in dirs, EAST in dirs, SOUTH in dirs, WEST in dirs), '?')
